69a9fb0fb6f1d6b8720c58fd01f7129e0a41ac62
syedshoaib02 <syeds7933.ss@gmail.com>
2023-11-30 16:15:09 +0530

    initial commit

--- data/database.js
+++ data/database.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose"
 
 
 export const connectDB =()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("server is coonected")})
+    mongoose.connect(process.env.MONGO_URI).then((c)=>{console.log(`server is coonected with ${c.connection.host}`)})
 }
